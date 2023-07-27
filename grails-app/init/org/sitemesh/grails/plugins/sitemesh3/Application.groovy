d1b971357c16717668ca3f365062b4b7be03637f
Scott Murphy <scott@alwaysvip.com>
2023-07-26 17:40:58 -0700

    grails create-web-plugin org.sitemesh.grails.plugins.sitemesh3.sitemesh3

---
+++ grails-app/init/org/sitemesh/grails/plugins/sitemesh3/Application.groovy
@@ -0,0 +1,13 @@
+package org.sitemesh.grails.plugins.sitemesh3
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+import groovy.transform.CompileStatic
+import grails.plugins.metadata.*
+
+@PluginSource
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
