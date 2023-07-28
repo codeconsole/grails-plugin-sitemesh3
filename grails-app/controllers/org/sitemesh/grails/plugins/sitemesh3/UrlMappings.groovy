1fa69143caebc537bb8ab6a86916088ab3fa0bc5
Scott Murphy <scott@alwaysvip.com>
2023-07-28 00:59:54 -0700

    remove unused mapping

--- grails-app/controllers/org/sitemesh/grails/plugins/sitemesh3/UrlMappings.groovy
+++ grails-app/controllers/org/sitemesh/grails/plugins/sitemesh3/UrlMappings.groovy
@@ -2,16 +2,10 @@ package org.sitemesh.grails.plugins.sitemesh3
 
 class UrlMappings {
     static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
         "/layouts/$name"(controller: 'layout', action: 'get')
 
         "/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
-
     }
 }
