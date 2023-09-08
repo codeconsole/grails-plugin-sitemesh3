6134ff657dbad3df37f6b636c952bfbd4df88857
Scott Murphy <scott@alwaysvip.com>
2023-09-08 18:31:51 -0500

    Document references. Remove constructor for stub.

--- grails-web-sitemesh2-adapter/src/main/java/org/grails/web/sitemesh/GrailsContentBufferingResponse.java
+++ grails-web-sitemesh2-adapter/src/main/java/org/grails/web/sitemesh/GrailsContentBufferingResponse.java
@@ -1,5 +1,9 @@
 package org.grails.web.sitemesh;
 
+// Needed by:
+// GrailsExceptionResolver (core)
+// GSPResponseWriter (gsp)
+
 public class GrailsContentBufferingResponse {
     public GrailsContentBufferingResponse() {}
 }
--- grails-web-sitemesh2-adapter/src/main/java/org/grails/web/sitemesh/GroovyPageLayoutFinder.java
+++ grails-web-sitemesh2-adapter/src/main/java/org/grails/web/sitemesh/GroovyPageLayoutFinder.java
@@ -4,6 +4,4 @@ public class GroovyPageLayoutFinder {
     public static final String LAYOUT_ATTRIBUTE = "org.grails.layout.name";
     public static final String NONE_LAYOUT = "_none_";
     public static final String RENDERING_VIEW_ATTRIBUTE = "org.grails.rendering.view";
-
-    public GroovyPageLayoutFinder() {}
 }
