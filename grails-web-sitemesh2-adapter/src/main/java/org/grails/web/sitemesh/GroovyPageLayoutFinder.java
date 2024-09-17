2731dad93fe12657a8efb19a38c4dbab0b062214
Scott Murphy Heiberg <scott@alwaysvip.com>
2024-09-16 22:02:36 -0700

    Adapter no longer needed with 7

--- grails-web-sitemesh2-adapter/src/main/java/org/grails/web/sitemesh/GroovyPageLayoutFinder.java
+++
@@ -1,9 +0,0 @@
-package org.grails.web.sitemesh;
-
-/*
-This is needed for compilation of the plugin, but not needed for distribution.
-Caused by: java.lang.NoClassDefFoundError: org/grails/web/sitemesh/GroovyPageLayoutFinder
-        at org.grails.plugins.web.interceptors.InterceptorArtefactHandler.<clinit>(InterceptorArtefactHandler.groovy:33)
- grails-core:trait Controller -> trait ResponseRenderer (GroovyPageLayoutFinder)
- */
-public class GroovyPageLayoutFinder {}
